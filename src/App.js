944f86f1b7290108be14cfeaff455241db8fd83e
Artluevil <artluevil139@gmail.com>
2019-08-16 18:03:29 +0200

    add some styling

--- src/App.js
+++ src/App.js
@@ -11,38 +11,74 @@ export default class App extends Component {
         })
     }
 
+    getStyleInput = () => {
+        if(this.state.theme === 'theme1') {
+            return 'themeInput1'
+        }
+        else if(this.state.theme === 'theme2') {
+            return 'themeInput2'
+        }
+        else if (this.state.theme === 'theme3') {
+            return 'themeInput3'
+        }
+    }
+
+    getStyleButton = () => {
+        if(this.state.theme === 'theme1') {
+            return 'themeButton1'
+        }
+        else if(this.state.theme === 'theme2') {
+            return 'themeButton2'
+        }
+        else if (this.state.theme === 'theme3') {
+            return 'themeButton3'
+        }
+    }
+
+    getBackground = () => {
+        if(this.state.theme === 'theme1') {
+            return 'background1'
+        }
+        else if(this.state.theme === 'theme2') {
+            return 'background2'
+        }
+        else if (this.state.theme === 'theme3') {
+            return 'background3'
+        }
+    }
+
     render() {
         console.log(this.state.theme)
         return (
-            <div>
+            <div className={this.getBackground()}>
                 <div className="buttons">
-                    <button name="theme1" onClick={this.onThemeClick}>Theme 1</button>
-                    <button name="theme2" onClick={this.onThemeClick}>Theme 2</button>
-                    <button name="theme3" onClick={this.onThemeClick}>Theme 3</button>
+                    <button name="theme1" onClick={this.onThemeClick}></button>
+                    <button name="theme2" onClick={this.onThemeClick}></button>
+                    <button name="theme3" onClick={this.onThemeClick}></button>
                 </div>
                 <div id="register-panel" className={this.state.theme}>
-                    <h1 style={{textAlign: 'center', marginBottom: '35px', paddingTop: '25px'}}>Registration Panel</h1>
+                    <h1 style={{textAlign: 'center', marginBottom: '35px', paddingTop: '25px', fontWeight: '10px'}}>Registration Panel</h1>
                     <div className="first-name">
-                        <input style={{textAlign: 'center'}} type="text" placeholder="First Name" />
+                        <input className={this.getStyleInput()} style={{textAlign: 'center'}} type="text" placeholder="First Name" />
                     </div>
                     <div className="last-name">
-                        <input style={{textAlign: 'center'}} type="text" placeholder="Last Name"/>
+                        <input className={this.getStyleInput()} style={{textAlign: 'center'}} type="text" placeholder="Last Name"/>
                     </div>
                     <div className="forms">
                         <div className="form">
-                            <input style={{marginTop: '35px', textAlign: 'center'}} type="text" placeholder="Password" />
+                            <input className={this.getStyleInput()} style={{marginTop: '35px', textAlign: 'center'}} type="text" placeholder="Password" />
                         </div>
                         <div className="form">
-                            <input style={{textAlign: 'center'}} type="text" placeholder="Repeat Password"/>
+                            <input className={this.getStyleInput()} style={{textAlign: 'center'}} type="text" placeholder="Repeat Password"/>
                         </div>
                         <div className="form">
-                            <input style={{textAlign: 'center'}} type="text" placeholder="Your Email"/>
+                            <input className={this.getStyleInput()} style={{textAlign: 'center'}} type="text" placeholder="Your Email"/>
                         </div>
                         <div className="form">
-                            <input style={{textAlign: 'center'}} type="text" placeholder="Security Question"/>
+                            <input className={this.getStyleInput()} style={{textAlign: 'center'}} type="text" placeholder="Security Question"/>
                         </div>
                         <div className="form">
-                            <input style={{textAlign: 'center'}} type="text" placeholder="Your Answer"/>
+                            <input className={this.getStyleInput()} style={{textAlign: 'center'}} type="text" placeholder="Your Answer"/>
                         </div>
                         <div style={{display: 'inline-block', marginLeft: '250px', marginTop: '25px'}}>
                             <label class="container">
@@ -57,7 +93,7 @@ export default class App extends Component {
                             </label>
                         </div>
                         <div className="register-btn">
-                            <button>Register</button>
+                            <button className={this.getStyleButton()}>Register</button>
                         </div>
                         <div className="styling-bar"></div>
                         <div className="genders">
